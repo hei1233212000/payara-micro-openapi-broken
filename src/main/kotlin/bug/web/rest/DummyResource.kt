69c824f357cc998c7466bd58958c51cfe3afc496
Harry Chan <harry.chan@mensa.org.hk>
2020-11-17 00:04:03 +0800

    updated DummyResource.kt to make the issue occurs

--- src/main/kotlin/bug/web/rest/DummyResource.kt
+++ src/main/kotlin/bug/web/rest/DummyResource.kt
@@ -3,6 +3,7 @@ package bug.web.rest
 import org.eclipse.microprofile.openapi.annotations.Operation
 import javax.enterprise.context.ApplicationScoped
 import javax.ws.rs.GET
+import javax.ws.rs.PUT
 import javax.ws.rs.Path
 
 @ApplicationScoped
@@ -11,4 +12,18 @@ class DummyResource {
     @GET
     @Operation(operationId = "dummy-operation", summary = "Test if OpenApi is working")
     fun hello() = "Hello"
+
+    @Path("/this-endpoint-cause-issue")
+    @PUT
+    fun updateSomething(requestBody: RequestBody) {
+        // issue would be occurs when the request body model contains an Enum field
+    }
+}
+
+class RequestBody {
+    var value: AnyEnum? = null
+}
+
+enum class AnyEnum {
+    A, B, C
 }
